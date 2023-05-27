fbd77a237c5b6071ecc97665b79460dca4a15ea6
Murilo Santos <132522384+MuriloSRuas@users.noreply.github.com>
2023-05-27 19:44:33 -0300

    Update exercicio4.py
    Esqueci de salvar o código , então eu copie e colei aqui XD

---
+++ exercicio4.py
@@ -0,0 +1,11 @@
+#Agora vamos formatar strings
+
+nome = "Murilo"
+idade = 17
+altura = 1.74
+peso = 53
+cor_de_olho = "castanho"
+tipo_de_fisico = "magro" 
+
+print (f"O meu nome é {nome} , tenho {idade} anos de idade , peso {peso} kg e tenho {altura} metros de altura")
+print (f"O meu olho é {cor_de_olho} e o meu físico é {tipo_de_fisico}")
