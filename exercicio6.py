3fb055373a1b9c2e9dd2939d21833ef7eb08a807
Murilo Santos <132522384+MuriloSRuas@users.noreply.github.com>
2023-08-15 18:37:50 -0300

    Exercício concluído
    Fiz o exercício 6 do livro learn_python (Strings e Format)

---
+++ exercicio6.py
@@ -0,0 +1,11 @@
+num1 = 5
+num2 = 25
+var_soma = num1 + num2
+num3 = [1 , 2 , 3 , 4 , 5]
+
+print (f"A soma de {num1} + {num2} é {var_soma}")
+
+#Esse format é bem legal de usar na linguagem das cobrassssssssssssssss
+#Bem facil de usar
+
+print (f"Olha só , vou somar todos os números do vetor 1 + 2 + 3 + 4 + 5 = {num3[0] + num3[1] + num3[2] + num3[3] + num3[4]}")
