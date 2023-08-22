9260da716f03c5bb4dfa04247a2018d0790d3d74
Murilo Santos <132522384+MuriloSRuas@users.noreply.github.com>
2023-08-22 19:40:48 -0300

    Erro no exercício 6
    O código antigo que estava aqui estava errado , esse exercício é o número 6 , só que errei e acabei colocando como exercício 7 . Agora sim está concertado

--- exercicio6.py
+++ exercicio6.py
@@ -1,11 +1,22 @@
-num1 = 5
-num2 = 25
-var_soma = num1 + num2
-num3 = [1 , 2 , 3 , 4 , 5]
+types_of_people = 10    #Declarei a variável do tipo inteiro
+x = f"Aqueles são os {types_of_people} tipos de pessoas"    #Declarei uma variavel do tipo string com o format
 
-print (f"A soma de {num1} + {num2} é {var_soma}")
+binary = "binário"   #declarei outra variavel do tipo inteiro
+do_not = "não"
+y = f"Aqueles que sabem {binary} e aqueles que {do_not} sabem"
 
-#Esse format é bem legal de usar na linguagem das cobrassssssssssssssss
-#Bem facil de usar
+print (x)
+print (y)
 
-print (f"Olha só , vou somar todos os números do vetor 1 + 2 + 3 + 4 + 5 = {num3[0] + num3[1] + num3[2] + num3[3] + num3[4]}")
+print (f"Eu disse : {x}")
+print (f"Eu também disse : '{y}'")
+
+hilarious = False
+joke_evaluation = "Aquela piada é muito engraçada ?! {}"
+
+print (joke_evaluation.format(hilarious))
+
+w = "Isso é o lado esquerdo de..."
+e = "uma string com um lado direito."
+
+print (w + e)
