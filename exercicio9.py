e06e6425e79fdffb243afa53b13cd21d9574fe9e
Murilo Santos <132522384+MuriloSRuas@users.noreply.github.com>
2023-09-16 16:03:41 -0300

    Exercício 9
    Fiz o exercício 9, dessa vez todo em inglês par dar uma praticada.

---
+++ exercicio9.py
@@ -0,0 +1,18 @@
+#According to the book, I'm going to write some new stranger codes, well, let's go so.
+
+days= "Monday Tuesday Wednesday Thursday Friday Saturday Sunday"
+months= "January\nFebruary\nMarch\nApril\nMay\nJune\nJuly\nAugust"  #The "\n" is for to break lines, pass for the next line.
+
+print("Here are the days: {}".format(days)) #XD
+print("Here are the months: {}".format(months),"\n")    #I used the format(), because I really liked them.
+
+print("""
+Frutas que eu mais gosto
+      
+-Banana
+-Abacaxi
+-Maçã
+-Pera
+-Mamão
+-Uva
+""")   #Do you know the diference between this three quotes print and the double quotes print? I know, with this print I can write with more lines.
