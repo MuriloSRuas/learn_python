52b964981d7a9b6ed8330e0e7900b0188e5598fd
Murilo Santos <132522384+MuriloSRuas@users.noreply.github.com>
2023-09-30 16:37:23 -0300

    Deletei exercicios12.py
    fiz o upload errado

--- exercicios12.py
+++
@@ -1,5 +0,0 @@
-sport= input("Which your favorite sport?")
-player= input("Which your favorite player?")
-wyl= input("What do you more like about him?")
-
-print(f"So, you like {sport}, your favorite player is {player} and what you more like about him is {wyl}")
