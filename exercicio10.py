1b7e71aed87184c3e25869a9fa4e75aa0c79c859
Murilo Santos <132522384+MuriloSRuas@users.noreply.github.com>
2023-09-30 16:36:15 -0300

    Exercicio 10, 11 e 12
    Exercícios 10, 11 e 12 foram feitos hj

---
+++ exercicio10.py
@@ -0,0 +1,13 @@
+#Let's go to code more exercises of python!!
+
+print("\"eu gosto de pão\".")    #The \" are double-quotes.
+print("\"eu também gosto de \'maçã\'\", se a maçã for sem aspas únicas eu nem como.")   #The \' are single-quotes.
+print("Ok, vamos comprar pão e maçã, então temos que comprar:\n*Pão\n*Maçã\nCerto?")    #"\n" is linefeed.
+print("\nBeleza \b, compramos o pão e\f a maçã. Agora é só \acomer\a")  #Hmm, I knew "\b" is backspace and "\f" is a recoil and... the "\a" I don't kwon what is.
+
+#I tried to use the "\N {name}", but don't work.
+
+print("O que é isso?\rpão de batata")   #What? I put the "\r" and it wrote "pão de batata" and don't wrote "O que é isso?"
+print("\tNesse mundo, existiu um homem que conquistou tudo na vida: fortuna, fama e poder. Foi Gold Roger, o 'Rei dos Piratas'")    #The "\t" is just tab
+print("A marinha tenta esconder o \v'D.' do nome de Roger.")    #The "\v" is vertical tab
+print("\x65")
---
+++ exercicio11.py
@@ -0,0 +1,19 @@
+print("How old are you?", end=' ')
+idade= input()
+print("How tall are you?", end=' ')
+height= input()
+print("How much do you weight?", end=' ')
+weight= input()
+
+print(f"So you're {idade} old, {height} tall and {weight} heavy.")
+
+#Others questions and inputs
+
+print("What is 2 + 2?", end=' ')
+num= int(input())
+print("When Stephen Curry borns?", end= ' ')
+year= int(input())
+
+print(f"Ok, it's {num} and Curry borns in {year}")
+
+input("Digite o seu nome13")
