d192b7d0dccbff70c90ad6e2616cac70d25e6c6c
Murilo Santos <132522384+MuriloSRuas@users.noreply.github.com>
2023-10-06 18:53:42 -0300

    Exercício 16
    Fiz o exercício 16

---
+++ exercicio16.py
@@ -0,0 +1,20 @@
+from sys import argv
+
+script, nome_texto= argv
+
+print("Vamos escrever!!")
+print (f"Agora, nós vamos escrever alguma coisa usando o python no seu texto vazio {nome_texto}")
+input("Vamo-nos?")
+
+escrever= open(nome_texto, "+w")
+
+print("Escreva alguma coisa no texto")
+seu_texto= input(">")
+
+escrever.write(seu_texto)
+escrever.close()
+
+print("Ficou assim:\n")
+ler= open(nome_texto, "r")
+print(ler.read())
+ler.close()
