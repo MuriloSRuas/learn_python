32f6cf96f7ecd9bae7a60961cda530f1947b6d97
Murilo Santos <132522384+MuriloSRuas@users.noreply.github.com>
2023-10-11 17:30:47 -0300

    Exercício 18
    Fiz o exercício 18

---
+++ exercicio18.py
@@ -0,0 +1,39 @@
+def checklist ():
+    print("Você iniciou a função com o def?")
+    input()
+
+    print("O nome da sua função só tem caracteres e _(underscore)?")
+    input()
+
+    print("Vocẽ abriu os parenteses depois do nome da função?")
+    input()
+    
+    print("Você colocou os args depois do parentese ( e separado por vírgula?")
+    input()
+
+    print("Vocẽ colocou cada variável com um nome único?")
+    input()
+
+    print("Você fechou o parentese ) e colocou os dois pontos depois dos args")
+    input()
+
+    print("Você colocou o código dentro da funcão com 1 tab de distância do começo da linha? Sem mais, nem menos")
+    input()
+
+    print("Você voltou a escrever de lá do começo da linha quando terminou a função?")
+    input()
+
+def checklist2():
+    print("Você rodou/chamou/usou a função chamando ela pelo nome?")
+    input()
+
+    print("Você colocou o parentese ( depois do nome da função?")
+    input()
+
+    print("Você colocou as variaveis que você queria dentro dos parênteses separados por vírgulas?")
+    input()
+
+    print("Você finalizou a chamada de função com parêntese )?")
+
+checklist()
+checklist2()
