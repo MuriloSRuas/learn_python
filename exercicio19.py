3f860707e2c84992376f7f529669c7ab62dbf3b6
Murilo Santos <132522384+MuriloSRuas@users.noreply.github.com>
2023-10-12 18:47:17 -0300

    Exercício 19
    Fiz o exercício 19

---
+++ exercicio19.py
@@ -0,0 +1,14 @@
+def colheres_de_mingau(colheres_de_mingau):
+    print(f"Estou fazendo um mingau em uma xícara com leite, vou colocar {colheres_de_mingau} colher de mingau e misturar.")
+    print(f"Ainda não está bom, vou colocar mais uma colher de mingau, até agora coloquei {colheres_de_mingau + 1} colheres de mingau.")
+    print(f"Ainda não está bom, falta só mais uma, então ao todo usei {colheres_de_mingau + 2} colheres de mingau.")
+    print("Agora é só comer.")
+
+#These things above is just some ways to call a function. 
+
+colheres_de_mingau(1)
+
+print(colheres_de_mingau(1))
+
+mingau = colheres_de_mingau(1)
+print(mingau)
