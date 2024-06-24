e2c0d0430fd2c426f3006bbb47cd9e13b0e1e304
Murilo Santos <murilosruas1@gmail.com>
2024-06-24 12:54:44 -0300

    Adicona comentários no exercício 42

--- ex42.py
+++ ex42.py
@@ -1,17 +1,19 @@
 class Creature:
-    #Creature is a class.
+    #Creature is-a class.
     def __init__(self, species : str, life : int, damage : int):
         self.species = species
         self.life = life
         self.damage = damage
 
 class Human(Creature):
+    #Human is-a Creature and has-a init and summary function; and the same attributes of Creature.
     def __init__(self, species, life, damage):
         super().__init__(species, life, damage)
-        #This code doesn't work, error: TypeError: __init__() missing 3 required positional arguments: 'species', 'life', and 'damage'
         
     def summary(self):
+        #Return Human attributes.
         return f"Specie: {self.species}, life: {self.life} and {self.damage}"
-        
+
+#The object "me" is-a Human and has-a specie, life and damage attributes.
 me = Human(species="Human", life=20, damage=10)
 print(me.summary())
