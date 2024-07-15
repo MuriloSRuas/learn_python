87cd2c85d530f2efea513af390631eb5deca1842
Murilo Santos <murilosruas1@gmail.com>
2024-07-15 13:16:06 -0300

    Adiciona configurador de ataque básico à ex44.py

--- ex44.py
+++ ex44.py
@@ -1,5 +1,4 @@
 from typing import Union
-from sys import exit
 
 class Brawler(object):
     def __init__(self, name : str, life : int, damage : int, trait : Union[str , None]):
@@ -11,5 +10,17 @@ class Brawler(object):
     def summary(self):
         print(f"{self.name}\n\nLife: {self.life}\nDamage: {self.damage}\n Trait: {self.trait}")
 
+    def set_BasicAttack(self):
+        name = input("What's the basic attack name?\n\n>")
+        damage = int(input(f"What's the damage of {name}?\n\n>"))
+        
+        basic_attack = {
+            "Name" : name,
+            "Damage" : damage
+        }
+
+        print("Here is the informations:\n\n{}".format(basic_attack))
+        return basic_attack
+
 Chester = Brawler("Chester", 7000, 1200, "Yes")
-Chester.summary()
+Chester.set_BasicAttack()
